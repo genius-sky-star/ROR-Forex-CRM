42337eadcfcbac004d2f45518d2311307d008302
boss-minion <lorenzoconti0917@gmail.com>
2023-10-14 10:40:10 -0500

    removed all

--- app/utils/build_aurora.sh
+++
@@ -1,8 +0,0 @@
-copilot storage init \
---name azara-ai-db \
---storage-type Aurora \
---workload azara-api \
---lifecycle environment \
---engine PostgreSQL \
---initial-db azara-api-db \
---serverless-version v2
